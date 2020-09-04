2702e529190c8a23c1150a2577c30adfa674cfdc
leonardobussi <leonardobezerrabussy@gmail.com>
2020-09-04 18:11:20 -0300

    add to viws for the project

--- src/routes/morador.js
+++ src/routes/morador.js
@@ -8,8 +8,13 @@ const router = express.Router();
 //router.get('/', Morador.getLogar);
 
 router.get('/', function(req, res){
-    return res.send("salve doido")
-  });
-
+    return res.render('visitante/index')
+});
+router.get('/sign', function(req, res){
+  return res.send("login morador")
+});
+router.get('/criar', function(req, res){
+  return res.send("criar logs para visitantes")
+});
 
 module.exports = router;
