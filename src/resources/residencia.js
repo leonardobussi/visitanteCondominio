752cbf35eb67ab6fde844af02e6ed202ff8aef1c
leonardobussi <leonardobezerrabussy@gmail.com>
2020-09-09 16:33:54 -0300

    opre

--- src/resources/residencia.js
+++ src/resources/residencia.js
@@ -7,14 +7,19 @@ const modelo2 =  mongoose.model('morador');
 class Residencia  {
     static async criar(dados){
 
-        const proj = await modelo2.findOne().populate('morador')
+        const proj = await modelo2.findOne().sort({nome_morador: 1})
+
+        console.log("a procura do id perfeito", proj)
         let {_id} = proj
+
+        console.log("esse era para ser o id perfeito", _id)
 
         let { morador } = dados;
         morador = _id;
         dados.morador = morador;
 
-        console.log(dados)
+        // /console.log(dados)
+        
         return await new  modelo(dados).save();
     }
 }
