960e12523e8084e502ad87e68261e43cccb15fed
Leonardo B. Bussi <leonardo.bussi@fatec.sp.gov.br>
2020-12-09 13:41:00 -0300

    Update keys.js

--- bin/keys.js
+++ bin/keys.js
@@ -5,7 +5,7 @@ module.exports = {
         port: process.env.PORT || 3000,
     },
     database: {
-        connection: 'mongodb+srv://rpg:rpg@cluster0.0ddez.mongodb.net/visita?retryWrites=true&w=majority' 
+        connection: '' 
     },
     auth: {
         secret: 'c1c2c3c4c5'
